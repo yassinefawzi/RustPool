d4632b3c2378de22212b44f17a2f0cc25b19954d
yassine fawzi <fawziyassine0@gmail.com>
2025-09-04 07:35:28 +0100

    quest 1

---
+++ current/target/debug/deps/quest1-9d1457ec2d2fdca7.d
@@ -0,0 +1,6 @@
+/home/yassine/Desktop/RustPool/quest1/target/debug/deps/quest1-9d1457ec2d2fdca7.d: src/main.rs src/tuples_refs.rs
+
+/home/yassine/Desktop/RustPool/quest1/target/debug/deps/libquest1-9d1457ec2d2fdca7.rmeta: src/main.rs src/tuples_refs.rs
+
+src/main.rs:
+src/tuples_refs.rs:
